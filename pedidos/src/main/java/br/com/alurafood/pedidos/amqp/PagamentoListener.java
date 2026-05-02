bd54d7298b96ea3c3cb70d62bd523271fae30877
Henrytos <henry.arcaya@sptech.school>
2026-05-02 01:21:06 -0300

    feat: recebendo mensagens com rabbitListener

---
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -0,0 +1,18 @@
+package br.com.alurafood.pedidos.amqp;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PagamentoListener {
+
+
+    @RabbitListener(queues = "pagamento.concluido")
+    public void recebeMensagem(
+            Message message
+    ) {
+        System.out.printf("Recebendo mensagem: %s%n", message.toString());
+    }
+
+}
